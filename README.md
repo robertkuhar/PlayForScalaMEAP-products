0024dd5a24c0ae67cf0f517a59f0afbe6094a514
Bob Kuhar <Robert_Kuhar@htc.com>
2013-06-28 12:59:10 -0700

    Complete Chapter 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 PlayForScalaMEAP-products
 =========================
 
-Play For Scala products demo application
+This is the products demo application implemented in Play 2.1 from the "Play 
+For Scala" MEAP by Peter Hilton, Erik Bakker, and Francisco Canedo.
+
--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -1,10 +1,18 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{ Action, Controller, Flash }
+import play.api.data.Form
+import play.api.data.Forms.{ mapping, longNumber, nonEmptyText }
+import play.api.i18n.Messages
 
 import models.Product
 
 object Products extends Controller {
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText)(Product.apply)(Product.unapply))
 
   def list = Action { implicit request =>
     val products = Product.findAll
@@ -17,4 +25,25 @@ object Products extends Controller {
     }.getOrElse(NotFound)
   }
 
+  def save = Action { implicit request =>
+    val newProductForm = this.productForm.bindFromRequest()
+
+    newProductForm.fold(
+      hasErrors = { form =>
+        Redirect(routes.Products.newProduct()).flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+      },
+      success = { newProduct =>
+        Product.add(newProduct)
+        Redirect(routes.Products.show(newProduct.ean))
+      })
+  }
+
+  def newProduct = Action { implicit request =>    
+    val form = if (flash.get("error").isDefined) {
+      this.productForm.bind(flash.data)
+    } else {
+      this.productForm
+    }
+    Ok(views.html.products.editProduct(form))
+  }
 }
